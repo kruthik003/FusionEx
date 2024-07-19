b61b6229e40e4de89bea9877709bf05d0140989b
Shreya Manchikanti <shreya.manchikanti@gmail.com>
2024-07-19 10:31:29 +0530

    Home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer";
+import Home from "./pages/Home/Home"
 
 function App() {
   return (
