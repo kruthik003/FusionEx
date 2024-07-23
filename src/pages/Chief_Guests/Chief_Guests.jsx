1e8bad6d8df7ecc30d8b9b08a02310e63836886c
Kruthik Juvventhula <106981407+kruthik003@users.noreply.github.com>
2024-07-23 14:17:31 +0530

    Initial Commit

--- src/pages/Chief_Guests/Chief_Guests.jsx
+++ src/pages/Chief_Guests/Chief_Guests.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
+import {useEffect,useState} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Chief_Guests.css'
 import images from '../../Image'
 import { faInstagram, faXTwitter } from '@fortawesome/free-brands-svg-icons'
 const Chief_Guests = () => {
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    setScrollPosition(window.scrollY > window.scrollX ? window.scrollY : window.scrollX);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  const opacity = Math.min(1,scrollPosition/300);
   return (
     <div className = 'chief_guests'>
       <div className = 'head'>
         Speakers
       </div>
       <div className = 'guests'>
-        <div className = 'guest_row'>
+        <div className = 'guest_row' style = {{opacity : opacity}}>
             <div className = 'guest_img'>
                 <img src = {images['Thub.JPG']} alt = ''></img>
             </div>
@@ -26,7 +42,7 @@ const Chief_Guests = () => {
                 <p>&nbsp;&nbsp;&nbsp;&nbsp; We are honored to welcome Srinivas Rao Mahankali, CEO of T-Hub, as our distinguished chief guest. Under his visionary leadership, T-Hub has become a premier innovation hub, driving transformative change in the startup ecosystem. His commitment to fostering entrepreneurship and technological advancement makes him an inspiring presence at our event.</p>
             </div>
         </div>
-        <div className = 'guest_row'>
+        <div className = 'guest_row' style = {{opacity : opacity}}>
             <div className = 'guest_img'>
                 <img src = {images['Citco.jpg']} alt = ''></img>
             </div>
@@ -42,7 +58,7 @@ const Chief_Guests = () => {
                 <p>&nbsp;&nbsp;&nbsp;&nbsp;Amit Verma, Country Head of Citco, is renowned for his exceptional leadership and strategic vision. His work has greatly enhanced Citco's presence in the financial sector. His expertise promises valuable insights and inspiration.</p>
             </div>
         </div>
-        <div className = 'guest_row'>
+        <div className = 'guest_row' style = {{opacity : opacity}}>
             <div className = 'guest_img'>
                 <img src = {images['KW.jpeg']} alt = ''></img>
             </div>
@@ -58,7 +74,7 @@ const Chief_Guests = () => {
                 <p>&nbsp;&nbsp;&nbsp;&nbsp;Khalid Wani is the Founder and CEO of KWCG, a dynamic leader renowned for his innovative vision and strategic expertise. Under his guidance, KWCG has achieved remarkable growth and established itself as a leader in the industry.</p>
             </div>
         </div>
-        <div className = 'guest_row'>
+        <div className = 'guest_row' style = {{opacity : opacity}}>
             <div className = 'guest_img'>
                 <img src = {images['AM.jpeg']} alt = ''></img>
             </div>
@@ -74,7 +90,7 @@ const Chief_Guests = () => {
                 <p>&nbsp;&nbsp;&nbsp;&nbsp;Amiya Mishra is the Vice President of Qualcomm, where he plays a pivotal role in driving technological advancements and strategic initiatives. With extensive expertise in the tech industry, Mishra is renowned for his leadership and innovative approach. His contributions continue to shape the future of technology on a global scale.</p>
             </div>
         </div>
-        <div className = 'guest_row'>
+        <div className = 'guest_row' style = {{opacity : opacity}}>
             <div className = 'guest_img'>
                 <img src = {images['NANI.jpg']} alt = ''></img>
             </div>
@@ -90,7 +106,7 @@ const Chief_Guests = () => {
                 <p>&nbsp;&nbsp;&nbsp;&nbsp; Nani, a celebrated actor in the Indian film industry, is known for his exceptional talent and versatility. With a string of successful films and a dedicated fan base, Nani continues to captivate audiences with his engaging performances and charismatic presence.</p>
             </div>
         </div>
-        <div className = 'guest_row'>
+        <div className = 'guest_row' style = {{opacity : opacity}}>
             <div className = 'guest_img'>
                 <img src = {images['Brahmi.jpg']} alt = ''></img>
             </div>
@@ -106,7 +122,7 @@ const Chief_Guests = () => {
                 <p>&nbsp;&nbsp;&nbsp;&nbsp;Brahmanandam, a legendary figure in Telugu cinema, is celebrated for his unparalleled comedic talent and impeccable timing. With a career spanning decades, he has delivered countless memorable performances, earning him a special place in the hearts of audiences. His wit and humor have made him a beloved icon in the film industry.</p>
             </div>
         </div>
-        <div className = 'guest_row'>
+        <div className = 'guest_row' style = {{opacity : opacity}}>
             <div className = 'guest_img'>
                 <img src = {images['SRV.jpeg']} alt = ''></img>
             </div>
@@ -122,7 +138,7 @@ const Chief_Guests = () => {
                 <p>&nbsp;&nbsp;&nbsp;&nbsp;Sandeep Reddy Vanga is a prominent film director renowned for his innovative storytelling and impactful narratives. His directorial ventures, including the critically acclaimed "Arjun Reddy", have redefined modern cinema. Vanga's creative vision and bold approach continue to inspire and influence the film industry.</p>
             </div>
         </div>
-        <div className = 'guest_row'>
+        <div className = 'guest_row' style = {{opacity : opacity}}>
             <div className = 'guest_img'>
                 <img src = {images['RANA.jpg']} alt = ''></img>
             </div>
